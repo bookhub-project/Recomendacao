c2bc0561f55be319b504d6081960a3935c535498
herodek <herodek_10@hotmail.com>
2019-10-26 21:45:01 -0300

    Primeiro Commit

---
+++ recomendacao/src/main/java/br/bookhub/Recomendador.java
@@ -0,0 +1,23 @@
+	package br.bookhub;
+
+		import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+		import org.apache.mahout.cf.taste.model.DataModel;
+
+			import java.io.File;
+			import java.io.IOException;
+//nesta classe está a relação dos modelos de filmes e livros para a serem recomendados
+			public class Recomendador {
+
+				public DataModel getModeloLivros() throws IOException {
+        //arquivo usado com a base de usuários, os códigos dos livros e as notas atribuídas
+					return getModelo("livros.csv");
+    }
+
+    		private DataModel getModelo(String path) throws IOException {
+    				File file = new File("src/main/resources/" + path);
+    				return new FileDataModel(file);
+    				}
+
+
+
+			}
